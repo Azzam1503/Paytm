7a18873565e8a399b4281c4e326f43a5403440a8
AZZAM UDDIN <uazzam69@gmail.com>
2024-04-06 02:41:08 +0530

    Intialized bank-webhook

---
+++ apps/bank-webhook/src/index.ts
@@ -0,0 +1,38 @@
+import express  from "express";
+import db from "@repo/db/client"
+
+const app = express();
+
+app.post("/bank", async (req, res) =>{
+    //TODO: Add zod validation
+    //Check if this request actually came from hdfc bank, use a webhook secret here 
+    const paymentInformation = {
+        token: req.body.token,
+        userId: req.body.user_identifier,
+        amount: req.body.amount
+    }
+
+    await db.balance.update({
+        where: {
+            userId: paymentInformation.userId
+        },
+        data: {
+            amount: {
+                increment: paymentInformation.amount
+            }
+        }
+    })
+
+    await db.onRampTransaction.update({
+        where: {
+            token: paymentInformation.token
+        },
+        data: {
+            status: "Success"
+        }
+    })
+
+    res.json({
+        message: "Captured"
+    })
+})
