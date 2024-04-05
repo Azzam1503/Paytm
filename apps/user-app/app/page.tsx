08e5b664075a0676ed549637aec2377653af29b7
AZZAM UDDIN <uazzam69@gmail.com>
2024-04-06 02:40:40 +0530

    Added authentication route

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,10 +1,12 @@
+"use client"
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Appbar } from "@repo/ui/appbar";
 
-
-export default function(){
-
-    return (
-        <div>
-           <h1>Root page</h1>
-        </div>
-    )
+export default function Page(): JSX.Element {
+  const session = useSession();
+  return (
+   <div>
+      <Appbar onSignin={signIn} onSignout={signOut} user={session.data?.user} />
+   </div>
+  );
 }
